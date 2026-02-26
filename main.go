ea59667ecf406dbdfe3138f57fc9a86e06e133dd
Guille Di Pa <guilledipasquale@gmail.com>
2026-02-26 13:22:39 +1100

    feat: Enhance watchdog configuration, update MediaMTX network bindings, and improve dashboard build process with cross-compilation.

--- main.go
+++ main.go
@@ -10,9 +10,9 @@ func main() {
 	fs := http.FileServer(http.Dir("./web"))
 	http.Handle("/", fs)
 
-	port := "80"
+	port := "8080"
 	fmt.Printf("BebeCam Dashboard running on http://localhost:%s\n", port)
-	err := http.ListenAndServe(":"+port, nil)
+	err := http.ListenAndServe("0.0.0.0:"+port, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
